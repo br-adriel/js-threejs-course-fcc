89133237467990f3a6b2fc1c6ba4a3441b566ba7
Adriel <adriel.fsantos@outlook.com>
2024-06-15 10:41:44 -0300

    feat: Add rotating colorful sphere

--- getting-started/main.js
+++ getting-started/main.js
@@ -13,5 +13,33 @@ const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 
+// esfera
+const geo = new THREE.IcosahedronGeometry(1, 3);
+
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading: true,
+});
+const mesh = new THREE.Mesh(geo, mat);
+
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+
 const scene = new THREE.Scene();
-renderer.render(scene, camera);
+mesh.add(wireMesh);
+scene.add(mesh);
+scene.add(hemiLight);
+
+function animate(t = 0) {
+  requestAnimationFrame(animate);
+  mesh.rotation.y = t * 0.0001;
+  renderer.render(scene, camera);
+}
+
+animate();
