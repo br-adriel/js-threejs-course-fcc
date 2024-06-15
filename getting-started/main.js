92a789a895fa032dda07b9aff2d0c1ad2231922a
Adriel <adriel.fsantos@outlook.com>
2024-06-15 11:03:49 -0300

    feat: Enable controls

--- getting-started/main.js
+++ getting-started/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -13,33 +14,39 @@ const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 
-// esfera
-const geo = new THREE.IcosahedronGeometry(1, 3);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+const geo = new THREE.IcosahedronGeometry(1, 5);
 
 const mat = new THREE.MeshStandardMaterial({
   color: 0xffffff,
   flatShading: true,
 });
-const mesh = new THREE.Mesh(geo, mat);
+const meshSphere = new THREE.Mesh(geo, mat);
 
 const wireMat = new THREE.MeshBasicMaterial({
   color: 0xffffff,
   wireframe: true,
 });
-const wireMesh = new THREE.Mesh(geo, wireMat);
-wireMesh.scale.setScalar(1.001);
+const wireMeshSphere = new THREE.Mesh(geo, wireMat);
+wireMeshSphere.scale.setScalar(1.001);
 
 const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+hemiLight.intensity = 5;
 
 const scene = new THREE.Scene();
-mesh.add(wireMesh);
-scene.add(mesh);
+meshSphere.add(wireMeshSphere);
+
+scene.add(meshSphere);
 scene.add(hemiLight);
 
 function animate(t = 0) {
   requestAnimationFrame(animate);
-  mesh.rotation.y = t * 0.0001;
+  meshSphere.rotation.y = t * 0.0001;
   renderer.render(scene, camera);
+  controls.update();
 }
 
 animate();
